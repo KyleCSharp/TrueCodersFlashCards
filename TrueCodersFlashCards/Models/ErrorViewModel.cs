0ff39d91dc3b3e4bbfa54cad3c712e9adb3a193e
Kylecsharp <kylelrigler@gmail.com>
2023-02-24 16:18:25 -0500

    deleted appsetting

---
+++ TrueCodersFlashCards/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrueCodersFlashCards.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
